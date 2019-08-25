91908cfccefcfc2ca9468bdb93ee392ee2f528fd
CRGT_Ghcoder <huan.gong@crgecent.com>
2019-08-25 18:49:23 +0800

    add leetcode 232

---
+++ LeetCode_232_用栈实现队列_97.14%.swift
@@ -0,0 +1,50 @@
+//
+//  LeetCode_232_用栈实现队列_97.14%.swift
+//  Coding
+//
+//  Created by Gonghuan on 2019/8/25.
+//  Copyright © 2019 Gonghuan. All rights reserved.
+//
+
+import Foundation
+
+
+class MyQueue {
+    
+    /** Initialize your data structure here. */
+    var datas: [Int] = []
+    init() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    func push(_ x: Int) {
+        self.datas.append(x)
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    func pop() -> Int {
+        guard !self.datas.isEmpty else { return -1 }
+        return self.datas.removeFirst()
+    }
+    
+    /** Get the front element. */
+    func peek() -> Int {
+        guard let firstObj = self.datas.first else { return -1 }
+        return firstObj
+    }
+    
+    /** Returns whether the queue is empty. */
+    func empty() -> Bool {
+        return self.datas.isEmpty
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * let obj = MyQueue()
+ * obj.push(x)
+ * let ret_2: Int = obj.pop()
+ * let ret_3: Int = obj.peek()
+ * let ret_4: Bool = obj.empty()
+ */
