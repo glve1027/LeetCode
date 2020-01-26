3a51894ef64316a5a768ebd1c95e21f2a0bc3ea8
Gonghuan <gonghuan1027@gmail.com>
2020-01-26 17:44:04 +0800

    add another method of leetcode 92

--- LeetCode_92. 反转链表 II_71.79%.swift
+++ LeetCode_92. 反转链表 II_71.79%.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// 方法一
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -55,5 +56,62 @@ class Solution {
         }
 
         return result
+    }
+}
+
+// 方法二
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func reverseBetween(_ head: ListNode?, _ m: Int, _ n: Int) -> ListNode? {
+        var index: Int = 1
+        let firstNode: ListNode = ListNode(-1)
+        var tempFirstNode: ListNode? = firstNode
+
+        var secondFirstNode: ListNode? = nil
+        var secondNode: ListNode? = nil
+
+        let lastNode: ListNode = ListNode(-1)
+        var tempLastNode: ListNode? = lastNode
+
+        var head = head
+
+        while head != nil {
+            if (index < m) {
+                tempFirstNode!.next = ListNode(head!.val)
+                tempFirstNode = tempFirstNode!.next
+            } else if (index >= m && index <= n) {
+                if (secondNode == nil) {
+                    secondNode = ListNode(head!.val)
+                    secondFirstNode = secondNode
+                } else {
+                    let temp = ListNode(head!.val)
+                    temp.next = secondNode
+
+                    secondNode = temp
+                }
+            } else {
+                tempLastNode!.next = ListNode(head!.val)
+                tempLastNode = tempLastNode!.next
+            }
+
+            head = head!.next
+            index += 1
+        }
+
+        // 链接
+        secondFirstNode?.next = lastNode.next
+        tempFirstNode!.next = secondNode
+
+        return firstNode.next
     }
 }
